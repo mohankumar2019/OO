f0d3f95bbd7b51210b1de43d968dadc60c721d8e
mohankumar2019 <50762193+mohankumar2019@users.noreply.github.com>
2019-08-27 12:37:37 +0530

    Update App.java

--- src/main/java/org/com/app/mvnOO/App.java
+++ src/main/java/org/com/app/mvnOO/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "mvnoo11111111111112222" );
+        System.out.println( "mvnoo1111111111111222222222222222222" );
     }
 }
